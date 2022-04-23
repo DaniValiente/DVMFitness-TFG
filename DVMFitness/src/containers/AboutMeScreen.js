6c7bf9698071eb3eed7fedd28425b61717ed9e46
Daniel Valiente Muñoz <dvaliente@grupoactive.es>
2022-04-23 11:21:53 +0200

    v1.01

--- DVMFitness/src/containers/AboutMeScreen.js
+++ DVMFitness/src/containers/AboutMeScreen.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { View, Text, FlatList, Image, RefreshControl } from "react-native";
+import { ScrollView, Text, FlatList, Image, RefreshControl } from "react-native";
 import body from "../styles/Body";
 function AboutMeScreen({ navigation }) {
     return (
-        <View style={body.contenedor}>
-        <Text>Settings screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => navigation.navigate('Details')}
-        />
-      </View>
+        <ScrollView style={body.contenedor}>
+        <Text>Mi nombre es Daniel Valiente, he sido el desarrollador de esta app enfocada en el mundo fitness, tengo 20 y soy sevillano.
+         Mi objetivo con esta aplicación era juntar y mezclar mis dos pasioness, la informatica y el mundo fitness </Text>
+         
+      </ScrollView>
     );
   }
   export default AboutMeScreen;
